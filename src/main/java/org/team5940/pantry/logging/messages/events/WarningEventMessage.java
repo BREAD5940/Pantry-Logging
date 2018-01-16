b126f22480229c702f1997ecaec9c9dfc157a38b
RaianLOL <rycen618@gmail.com>
2018-01-16 09:57:38 -0800

    added throwable improvement to WarningEventMessage

--- src/main/java/org/team5940/pantry/logging/messages/events/WarningEventMessage.java
+++ src/main/java/org/team5940/pantry/logging/messages/events/WarningEventMessage.java
@@ -33,5 +33,15 @@ public class WarningEventMessage extends EventMessage {
 		this(source, name);
 		this.getData().add("throwable", LoggingUtils.getObjectInfoAsJson(throwable));
 	}
+	
+	/**
+	 * Constructor for the warning event message with the throwable passed. Adds the object info of the throwable as a JsonObject which includes the "message" and "stack_trace"
+	 * The name for this is the message in the throwable. 
+	 * @param source The source of the message.
+	 * @param throwable The throwable that is to be logged.
+	 */
+	public WarningEventMessage(Object source, Throwable throwable) {
+		this(source, throwable.getMessage(), throwable);
+	}
 
 }
